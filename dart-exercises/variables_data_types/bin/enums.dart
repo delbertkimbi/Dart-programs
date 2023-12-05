851f45264289769c7ca70dc6fc936af630d03584
Delbert <delbertdrums5@gmail.com>
2023-12-05 05:49:53 +0100

    Errors of multiCase printing resolved

--- dart-exercises/variables_data_types/bin/enums.dart
+++ dart-exercises/variables_data_types/bin/enums.dart
@@ -17,8 +17,10 @@ void main() {
   String day = stdin.readLineSync()!; // reads a non nullable string
 
   /// Getting the number which represents the day of the week
+  int dayNum;
   print("Enter a number that represents the day");
-  int dayNum = int.parse(stdin.readLineSync()!); //reads non nullable number
+  dayNum = int.parse(stdin.readLineSync()!);             //reads non nullable number
+
 
   //Checking if dayNum is between 1 and 7
   assert(dayNum >= 1 && dayNum <= 7);
@@ -32,28 +34,33 @@ void main() {
 
   ///forEach repeatition control structure of enums
   DaysOfTheWeek.values.forEach((i) {
-    i.name.toUpperCase() == day.toUpperCase()
+    i.name.toUpperCase() ==
+            day.toUpperCase() // Preventing differences due to cases
         ? print("${i.name.toUpperCase()} is the ${dayNum} day of the week.")
         : null;
-    switch (i.index) {
-      case 0:
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-        print(
-            "${i.name} is a school day. So make sure to go to school so as to learn something profitable");
-        break;
-      case 5:
-        print(
-            "${i.name} is your sports day and chilling day. Make sure to have fun!!!");
-        break;
-      case 6:
-        print(
-            "${i.name} is a sabath day, so make sure you keep it holy by going to church");
-        break;
-      default:
-        print("Something is wrong some where.");
+
+    // checking to make sure the index equalls the dayNum-1 to prevent printing all cases
+    if (i.index == (dayNum - 1)) {
+      switch (i.index) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+          print(
+              "${i.name} is a school day. So make sure to go to school so as to learn something profitable");
+          break;
+        case 5:
+          print(
+              "${i.name} is your sports day and chilling day. Make sure to have fun!!!");
+          break;
+        case 6:
+          print(
+              "${i.name} is a sabath day, so make sure you keep it holy by going to church");
+          break;
+        default:
+          print("Something is wrong some where.");
+      }
     }
   });
 }
