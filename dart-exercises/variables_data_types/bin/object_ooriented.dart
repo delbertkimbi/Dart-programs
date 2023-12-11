5a1f93c8bcacb27751bc844a023d46c4990c7709
Delbert <delbertdrums5@gmail.com>
2023-12-11 04:20:21 +0100

    Highleted topics to learn this week.

---
+++ dart-exercises/variables_data_types/bin/object_ooriented.dart
@@ -0,0 +1,16 @@
+void main() {}
+ // Classes
+
+ /// Constructors andd properties
+
+
+ ///Getters and setters 
+ ///Methods and properties
+ /// Object oriented Principles
+          //Abstration
+          //Inheritance
+          //Polymorphism
+          //Encapsulation
+          //Resusability (Generics)
+ 
+ 
