b821db37e2fd9127ff7e5d809f43329fdd43797f
Delbert <delbertdrums5@gmail.com>
2023-12-28 17:27:16 +0100

    Streams in dart

--- dart-exercises/variables_data_types/bin/Advanced stuff/streams.dart
+++ dart-exercises/variables_data_types/bin/Advanced stuff/streams.dart
@@ -1,3 +1,18 @@
-void main(){
-  
+void main() {
+  test();
+}
+
+Stream<String> periodicStream() {
+  return Stream.periodic(
+    Duration(seconds: 1),
+    (value) {
+      return "Delbert is wealthy";
+    },
+  );
+}
+
+void test() async {
+  await for (final value in periodicStream()) {
+    print(value);
+  }
 }
