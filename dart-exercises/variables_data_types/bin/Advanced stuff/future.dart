90ac1c3b3a2db04b6143bde413a0613ff38745bf
Delbert <delbertdrums5@gmail.com>
2023-12-28 17:08:08 +0100

    Asynnchrous programs

---
+++ dart-exercises/variables_data_types/bin/Advanced stuff/future.dart
@@ -0,0 +1,14 @@
+void main() {}
+Future<int> futureMultiples(int a) {
+  return Future.delayed(
+    Duration(seconds: 3),
+    () {
+      return 2 * a;
+    },
+  );
+}
+
+void test() async {
+  int r = await futureMultiples(4);
+  print("\nThe future results is $r");
+}
---
+++ dart-exercises/variables_data_types/bin/mixins.dart
@@ -0,0 +1,19 @@
+void main() {}
+
+class Instruments with Sounds {
+  
+}
+
+mixin Sounds {
+  void commonAttributes() {
+    print("I have the marimber and Ocarina sound:\n");
+  }
+}
+
+class Piano {
+   
+}
+
+class SoloGuiter {
+
+}
