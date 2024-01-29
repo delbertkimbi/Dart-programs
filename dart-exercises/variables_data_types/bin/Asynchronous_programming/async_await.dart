90bd490f528c1d795e38d5440c003d5f2592fdf0
Delbert <delbertdrums5@gmail.com>
2024-01-29 15:17:52 +0100

    Asynchronous programming

--- dart-exercises/variables_data_types/bin/Asynchronous_programming/async_await.dart
+++ dart-exercises/variables_data_types/bin/Asynchronous_programming/async_await.dart
@@ -1,36 +1,37 @@
+import "dart:async";
+
 // Part 1
 // You can call the provided async function fetchRole()
 // to return the user role.
 Future<String> reportUserRole() async {
-  var descript= await UserRole();
+  var descript = await userRole();
   return descript;
 }
-Future<String> UserRole(){
-  Future.delayed(
-    const Duration(seconds: 3),
-    ()=> "A tech enthusiast with lots of cool idea in mind to implement and make the world a better place."
-  );
+
+Future<String> userRole() {
+  return Future.delayed(
+      const Duration(seconds: 3),
+      () =>
+          "A tech enthusiast with lots of cool idea in mind to implement and make the world a better place.");
 }
+
 // Part 2
 // Implement reportLogins here
 // You can call the provided async function fetchLoginAmount()
 // to return the number of times that the user has logged in.
-Future<int> fetchLoginAmount() async{
-  int amount =await reportLogins();
+Future<int> fetchLoginAmount() async {
+  int amount = await reportLogins();
   return amount;
 }
-Future<int>reportLogins() 
-{
-  Future.delayed(
-    const duration(seconds:2),
-    ()=> 5
-  );
+
+Future<int> reportLogins() {
+ return Future.delayed(const Duration(seconds: 2), () => 5);
 }
 
-Future<void> main() async{
-  List waiter=[
-    fetchLoginAmount(),
-    reportUserRole(),
+Future<void> main() async {
+  List waiter = [
+    await fetchLoginAmount(),
+    await reportUserRole(),
   ];
-  print(await waiter);
+  print( waiter);
 }
