a4c4461a52eb623a65971a0a0cec13cdf8cbde31
Delbert <delbertdrums5@gmail.com>
2024-01-29 06:14:16 +0100

    Asynchronous use of try/catch

---
+++ dart-exercises/variables_data_types/bin/Asynchronous_programming/tempCodeRunnerFile.dart
@@ -0,0 +1,3 @@
+
+  var name=Future.delayed(
+    const Duration
--- dart-exercises/variables_data_types/bin/Asynchronous_programming/try_catch.dart
+++ dart-exercises/variables_data_types/bin/Asynchronous_programming/try_catch.dart
@@ -1,18 +1,19 @@
-
+import "dart:async";
 //Use async and await to implement an asynchronous changeUsername() function that does the following:
   //Calls the provided asynchronous function fetchNewUsername() and returns its result.
       //Example return value from changeUsername(): "jane_smith_92"
   //Catches any error that occurs and returns the string value of the error.
   //You can use the toString() method to stringify both Exceptions and Errors.
 Future<String> fetchNewUserName()async{
-  var username=await NewUserName();
+  var username=await newUserName();
   return username;
 }
-Future<String> NewUserName(){
-  String name=Future.delayed(
+Future<String> newUserName(){
+  var name=Future.delayed(
     const Duration(seconds:3),
     ()=> "delbert_Kimbi_19",
   );
+  return name;
 }
 // Implement changeUsername here
 //Returns the new username that you can use to replace an old one.
@@ -22,6 +23,10 @@ Future<String> changeUsername() async{
    return change;
  }
   catch(e){
-     print("The error:${e.toString()}");
+     return "The error:${e.toString()}";
   }
+}
+void main()async{
+  print("Waiting for some responds...");
+  print(await changeUsername());
 }
